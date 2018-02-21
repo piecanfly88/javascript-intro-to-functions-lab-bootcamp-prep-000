67abce76fd59320db43656a89eaee6c9e80470c2
Richard Salchunas <sccrlx4ss2@yahoo.com>
2018-02-21 03:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()) {
+    console.log("I can't hear you!");
+  } else if (string.toUpperCase()) {
+    console.log("YES INDEED!");
+  } else if (string === "I love you, Grandma.") {
+    console.log("I love you, too.")
+  }
+}
