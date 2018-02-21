23c1aa95309fa8dd62370622741afdf5b9c1caed
Richard Salchunas <sccrlx4ss2@yahoo.com>
2018-02-21 03:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
-    console.log("I can't hear you!");
+    return "I can't hear you!";
   } else if (string.toUpperCase()) {
-    console.log("YES INDEED!");
+    return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
-    console.log("I love you, too.")
+    return "I love you, too.";
   }
 }
